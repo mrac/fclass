af03a756294d2836113555ec7e4bad9bc77d43f0
Michal Raczkowski <michal.raczkowski@gmail.com>
2015-05-29 08:44:22 +0200

    access property by value

--- src/firstclass.ts
+++ src/firstclass.ts
@@ -2,19 +2,43 @@
 type Function1d = (element: any, index: number, array: Array<any>) => any;
 
 
-(function() {
+(function () {
   
-  this.property = function(key: string, value?: any): Function1d {
+  this.property = function (key: string, value?: any): Function1d {
+    if(key) {
+      if(typeof value !== 'undefined') {
+        return function (e) {
+          return e[key] === value ? e[key] : null;
+        };
+      } else {
+        return function (e) {
+          return key in e ? e[key] : null;
+        };
+      }
+    } else {
+      if(typeof value !== 'undefined') {
+        return function (e) {
+          var key;
+          var found = Object.keys(e).some(function (k) {
+            key = k;
+            return e[k] === value;
+          });
+          return found ? e[key] : null;
+        };
+      }
+    }
+  };
+
+  this.object = function (key: string, value?: any): Function1d {
     if(typeof value !== 'undefined') {
-      return function(e) {
-        return e[key] === value ? e[key] : null;
+	  return function (e) {
+		return e[key] === value ? e : null;
       };
     } else {
-      return function(e) {
-        return key in e ? e[key] : null;
+      return function (e) {
+        return key in e ? e : null;
       };
     };
   };
-
 
 }.call(this));
--- test/firstclass-spec.js
+++ test/firstclass-spec.js
@@ -1,7 +1,6 @@
 describe('property()', function() {
 
-
-  it('should return function for accessing property value', function() {
+  it('should return function for accessing property by key', function() {
     var obj = {a: 10, b: 5};
     expect(property('a')(obj)).toEqual(10);
     expect(property('b')(obj)).toEqual(5);
@@ -12,8 +11,7 @@ describe('property()', function() {
     expect(arr.filter(property('a'))).toEqual([{a: 10, b: 5}, {a: 7, b: 5}, {a: 10, b: 7}]);
   });
 
-
-  it('should return function for filtering property value', function() {
+  it('should return function for accessing property by key,value', function() {
     var obj = {a: 10, b: 5};
     expect(property('a', 10)(obj)).toEqual(10);
     expect(property('b', 5)(obj)).toEqual(5);
@@ -26,5 +24,44 @@ describe('property()', function() {
     expect(arr.filter(property('a', 10))).toEqual([{a: 10, b: 5}, {a: 10, b: 7}]);
   });
 
+  it('should return function for accessing property by value', function() {
+    var obj = {a: 10, b: 5};
+    expect(property(null, 10)(obj)).toEqual(10);
+    expect(property(null, 5)(obj)).toEqual(5);
+    expect(property(null, 7)(obj)).toEqual(null);
+    
+    var arr = [{a: 10, b: 5}, {a: 7, b: 5}, {a: 10, b: 7}, {w: 10}];
+    expect(arr.map(property(null, 10))).toEqual([10, null, 10, 10]);
+    expect(arr.filter(property(null, 10))).toEqual([{a: 10, b: 5}, {a: 10, b: 7}, {w: 10}]);
+  });
+
+});
+
+
+describe('object()', function() {
+
+  it('should return function for accessing object by key', function() {
+    var obj = {a: 10, b: 5};
+    expect(object('a')(obj)).toEqual({a: 10, b: 5});
+    expect(object('b')(obj)).toEqual({a: 10, b: 5});
+    expect(object('w')(obj)).toEqual(null);	
+    
+    var arr = [{a: 10, b: 5}, {a: 7, b: 5}, {a: 10, b: 7}, {w: 100}];
+    expect(arr.map(object('a'))).toEqual([{a: 10, b: 5}, {a: 7, b: 5}, {a: 10, b: 7}, null]);
+    expect(arr.filter(object('a'))).toEqual([{a: 10, b: 5}, {a: 7, b: 5}, {a: 10, b: 7}]);
+  });
+
+  it('should return function for accessing object by key,value', function() {
+    var obj = {a: 10, b: 5};
+    expect(object('a', 10)(obj)).toEqual({a: 10, b: 5});
+    expect(object('b', 5)(obj)).toEqual({a: 10, b: 5});
+    expect(object('a', 5)(obj)).toEqual(null);
+    expect(object('b', 7)(obj)).toEqual(null);
+    expect(object('w', 1)(obj)).toEqual(null);
+    
+    var arr = [{a: 10, b: 5}, {a: 7, b: 5}, {a: 10, b: 7}, {w: 100}];
+    expect(arr.map(object('a', 10))).toEqual([{a: 10, b: 5}, null, {a: 10, b: 7}, null]);
+    expect(arr.filter(object('a', 10))).toEqual([{a: 10, b: 5}, {a: 10, b: 7}]);
+  });
 
 });
